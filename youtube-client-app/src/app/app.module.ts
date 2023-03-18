03af9b197260048d26d79ef79ba0b06b523ad5f0
Julia Kupreichik <julilike@yandex.ru>
2023-03-18 19:01:53 +0300

    feat: cenerate components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { SearchFormComponent } from './components/header/search-form/search-form.component';
+import { SortFormComponent } from './components/header/sort-form/sort-form.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
+import { SearchItemComponent } from './components/search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    SortFormComponent,
+    SearchResultsComponent,
+    SearchItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ youtube-client-app/src/app/components/header/sort-form/sort-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-form',
+  templateUrl: './sort-form.component.html',
+  styleUrls: ['./sort-form.component.scss']
+})
+export class SortFormComponent {
+
+}

a39f64bf69e02117d7d90dfee55723f3973536cb
Julia Kupreichik <julilike@yandex.ru>
2023-04-06 22:24:57 +0300

    feat: add card create form with validations

--- youtube-client-app/src/app/youtube/pages/admin-page/admin-page.component.ts
+++ youtube-client-app/src/app/youtube/pages/admin-page/admin-page.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, FormControl } from '@angular/forms';
+import { urlPattern } from '../../constants/regexp-patterns.constant';
+import { ValidateService } from '../../services/validate/validate.service';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+  styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
+  cardCreateForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private validateService: ValidateService) {}
+
+  ngOnInit(): void {
+    this.cardCreateForm = this.formBuilder.group({
+      cardTitle: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      cardDescription: ['', [Validators.maxLength(255)]],
+      cardImg: ['', [Validators.required, Validators.pattern(urlPattern)]],
+      cardLink: ['', [Validators.required, Validators.pattern(urlPattern)]],
+      cardDate: ['', [Validators.required, this.validateService.dateValidator]],
+    });
+  }
+
+  get cardTitle() {
+    return this.cardCreateForm.get('cardTitle');
+  }
+
+  get cardDescription() {
+    return this.cardCreateForm.get('cardDescription');
+  }
+
+  get cardImg() {
+    return this.cardCreateForm.get('cardImg');
+  }
+
+  get cardLink() {
+    return this.cardCreateForm.get('cardLink');
+  }
+
+  get cardDate() {
+    return this.cardCreateForm.get('cardDate');
+  }
 }

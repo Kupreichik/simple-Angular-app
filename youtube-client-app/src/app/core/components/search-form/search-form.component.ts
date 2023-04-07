5fc84d2ee78804af848491798e9f3f65d88b91e7
Julia Kupreichik <julilike@yandex.ru>
2023-04-07 16:15:37 +0300

    feat: implement search input as observable with debaunce

--- youtube-client-app/src/app/core/components/search-form/search-form.component.ts
+++ youtube-client-app/src/app/core/components/search-form/search-form.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search/search.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter } from 'rxjs';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.scss'],
 })
-export class SearchFormComponent {
-  searchingText = '';
+export class SearchFormComponent implements OnInit {
+  searchInput = new FormControl;
 
   constructor(private searchService: SearchService) {}
 
-  submitSearchingText() {
-    this.searchService.setSearchText(this.searchingText);
+  ngOnInit(): void {
+    this.searchInput.valueChanges.pipe(
+      filter(value => value.length >= 3),
+      debounceTime(500)
+    ).subscribe(
+      (value) => this.searchService.setSearchText(value)
+    )
   }
 }
